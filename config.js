7cf731543e9dd1d8e4b1ef8698f1ed5779850a5f
Atharv <92430534+Atharv3142M@users.noreply.github.com>
2025-08-29 18:47:38 +0530

    Create config.js

---
+++ config.js
@@ -0,0 +1,5 @@
+// config.js
+const CONFIG = {
+  API_KEY: "AIzaSyBTVp15nqxHBEBrL8g3BQlJyWGmUgIrjjA",  // Replace with your restricted key
+  FOLDER_ID: "1YJ416WrYlXCWmFtCXJpxV3x1sjtwXegq" // Replace with your folder ID
+};
